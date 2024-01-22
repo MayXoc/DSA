0d0209e38d3a07111e519d5cd287ba7f33f616b8
MayXoc <mayankbisht8126@gmail.com>
2024-01-22 16:46:56 +0530

    BinarySearchInNearlyShortredArrray.cpp

---
+++ BinarySearchInNearlyShortredArrray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool FindIndex(vector <int> &arr,int target){
+    int s=0;
+    int e = arr.size()-1;
+    int mid = s+(e-s)/2;
+    while(s<=e){
+        if(target==arr[mid] || target==arr[mid-1] || target==arr[mid+1]){
+            return true;
+        } 
+        else if(target>arr[mid]){
+            s=mid+2;
+        } 
+        else  e=mid-2;
+        mid = s+(e-s)/2;
+    }
+    cout<<"False";
+    return false;
+}
+int main(){
+    vector <int> arr{20,10,30,50,40,70,60} ;
+    int target = 70;
+    FindIndex(arr,target);   
+}
