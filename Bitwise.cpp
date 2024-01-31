af160bec5c523ce5299352f83ba1d20305d1d98c
Mayank Bisht <156527603+MayXoc@users.noreply.github.com>
2024-01-31 22:48:30 +0530

    Update Bitwise.cpp

--- Bitwise.cpp
+++ Bitwise.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 int a=7;
 int main(){
-//	int a=5;
-//	int b=6;
-//	cout<<(5^5^5)<<endl;
-//	cout<<(7 << 1);
-//cout<<(-50 >> 1);
-//unsigned int a =-50;
-//cout<<(a>>1);
+	int a=5;
+	int b=6;
+	cout<<(5^5^5)<<endl;
+	cout<<(7 << 1);
+cout<<(-50 >> 1);
+unsigned int a =-50;
+cout<<(a>>1);
 int a=10;
 cout<<((++a)*(a++));
-//int a=9;
-//cout<<::a;
-//cout<<"\n"<<a;	
-//for(int i=0;i<5;i++){
-//
-//	if(i==1){
-//		cout<<i;
-//		continue;
-//		cout<<i+1<<"*\n";
-//	}
-//	cout<<i;
-//}
+int a=9;
+cout<<::a;
+cout<<"\n"<<a;	
+for(int i=0;i<5;i++){
+
+	if(i==1){
+		cout<<i;
+		continue;
+		cout<<i+1<<"*\n";
+	}
+	cout<<i;
+}
 }
