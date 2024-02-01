dea9dcf06883baee32b9a7233bee3f6656a69201
Mayank Bisht <156527603+MayXoc@users.noreply.github.com>
2024-02-01 22:05:11 +0530

    Update Bitwise.cpp

--- Bitwise.cpp
+++ Bitwise.cpp
@@ -4,15 +4,15 @@ int a=7;
 int main(){
 	int a=5;
 	int b=6;
-	cout<<(5^5^5)<<endl;
-	cout<<(7 << 1);
+	cout<<(5^5^5)<<endl;  //using xor operator
+	cout<<(7 << 1); //shifting 7 by one place
 cout<<(-50 >> 1);
 unsigned int a =-50;
 cout<<(a>>1);
 int a=10;
 cout<<((++a)*(a++));
 int a=9;
-cout<<::a;
+cout<<::a; // accesing global variable a 
 cout<<"\n"<<a;	
 for(int i=0;i<5;i++){
 
