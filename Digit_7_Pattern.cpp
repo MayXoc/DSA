e828b31e29842048f371fc5c45004a4a2cfaeef5
Mayank Bisht <156527603+MayXoc@users.noreply.github.com>
2024-02-01 22:02:00 +0530

    Update Digit_7_Pattern.cpp

--- Digit_7_Pattern.cpp
+++ Digit_7_Pattern.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main (){
 	for(int i = 0;i<5;i++){
 		for(int j = 0;j<5;j++){
-			if(i==0 || (i==1 && j==3) || (i==2 && j==2) || (i==3 && j==1) || (i==4 && j==0)){
+			if(i==0 || (i==1 && j==3) || (i==2 && j==2) || (i==3 && j==1) || (i==4 && j==0)){  //printing digit 7
 				cout<<"7"<<" ";
 			}
 			else{
-				cout<<" "<<" "; 
+				cout<<" "<<" "; //printing spaces
 			}
 		}
 		cout<<endl;
