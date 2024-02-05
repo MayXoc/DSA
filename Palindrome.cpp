702458189b97f83f10c548bc6fc176ce90c7e671
MayXoc <mayankbisht8126@gmail.com>
2024-02-05 13:11:54 +0530

    Updated

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+void UpperToLower(char ch[]){
+    int index =0;
+    while(ch[index]!='\0'){
+       
+        if(ch[index]>='A' && ch[index]<='Z'){
+            ch[index] = 'a' + (ch[index]-'A');
+        }
+        index++;
+    }
+}
 int findLength(char ch[],int n){
     int index = 0;
     while(ch[index]!='\0'){
@@ -7,6 +17,7 @@ int findLength(char ch[],int n){
     return index;
 }
 void Palindrome(char ch[],int len){
+    UpperToLower(ch);
     //using two pointer approach to compare and find the palindrome ;
     int s=0;
     int e=len-1;
