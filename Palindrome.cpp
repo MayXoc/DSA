f7ef38a647d6fcbb7ddae3935d0e36476dfdb415
MayXoc <mayankbisht8126@gmail.com>
2024-02-05 12:53:57 +0530

    Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+int findLength(char ch[],int n){
+    int index = 0;
+    while(ch[index]!='\0'){
+        index++;
+    }
+    return index;
+}
+void Palindrome(char ch[],int len){
+    //using two pointer approach to compare and find the palindrome ;
+    int s=0;
+    int e=len-1;
+    bool ans=true;;
+    while(s<e){
+        if(ch[s]==ch[e]){
+            s++;
+            e--;
+        }
+        else{
+            ans=false;
+            break;
+        }
+    }
+    if(ans==true){
+        std::cout<<"Given string is palindrome";
+    }
+    else{
+        std::cout<<"Given string is not palindrome";
+    }
+}
+int main(){
+    char ch[100];
+    std::cout<<"Enter your string - ";
+    std::cin.getline(ch,100);
+    int length = findLength(ch,100);
+    //function for checking it is palindrome or not 
+    Palindrome(ch,length);
+}
