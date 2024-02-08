67afdc9021f94ea33ef00956d11de7d71db3d927
MayXoc <mayankbisht8126@gmail.com>
2024-02-08 18:28:56 +0530

    updated + (BinaryandTwoPointer)approach added

--- K-Closest_ElementLeetcode.cpp
+++ K-Closest_ElementLeetcode.cpp
@@ -4,25 +4,68 @@
 #include <algorithm>
 #include <climits>
 using namespace std;
-void TwoPointerApproach(vector<int>&arr,int k,int x){
-        int l=0,h=arr.size()-1;
-        vector<int>ans;
-        while(h-l>=k){
-            if(x-arr[l]>arr[h]-x){
-                l++;
-            }
-            else{
-                h--;
-            }
+int lowerBound(vector <int> &arr,int x){
+    int s=0,e=arr.size()-1;
+    int ans;
+    while(s<=e){
+        int mid = s+(e-s)/2;
+        if(arr[mid]>=x){
+            ans=mid;
+            e=mid-1;
         }
-        for(int i=l;i<=h;i++){
+        else if (arr[mid]<x){
+            s= mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+    }
+    return ans;
+}
+void BinarySearch_TwoPointer(vector <int> &arr,int k,int x){
+    vector <int>ans;
+    int h = lowerBound(arr,x);
+    int l = h-1;
+    while(k--){
+        if(l<0){
+            h++;
+        }
+        else if(h>arr.size()){
+            l--;
+        }
+        else if(x-arr[l]>arr[h]-x){
+            h++;
+        }
+        else{
+            l--;
+        }
+    }
+      for(int i=l+1;i<=h-1;i++){
             ans.push_back(arr[i]);
         }
-        
-         for(auto v : ans){
-           cout<<v<<" ";
-        }
+    for(auto v : ans){
+        cout<<v<<" ";
+    }
 }
+// void TwoPointerApproach(vector<int>&arr,int k,int x){
+//         int l=0,h=arr.size()-1;
+//         vector<int>ans;
+//         while(h-l>=k){
+//             if(x-arr[l]>arr[h]-x){
+//                 l++;
+//             }
+//             else{
+//                 h--;
+//             }
+//         }
+//         for(int i=l;i<=h;i++){
+//             ans.push_back(arr[i]);
+//         }
+        
+//          for(auto v : ans){
+//            cout<<v<<" ";
+//         }
+// }
 
 // int index(vector <int> &arr){
 //       auto it = min_element(arr.begin(), arr.end());
@@ -33,7 +76,7 @@ void TwoPointerApproach(vector<int>&arr,int k,int x){
 // }
 
 int main() {
-    vector <int> arr{1,2,3,4,5};
+    vector <int> arr{2,3,4,5,6};
     int size = arr.size()-1;
     int k = 4,x=3;
     // int n=k;
@@ -52,6 +95,8 @@ int main() {
     // for(auto v : ans){
     //     cout<<v<<" ";
     // }
-    TwoPointerApproach(arr,k,x);
+    // TwoPointerApproach(arr,k,x);
+    BinarySearch_TwoPointer(arr,k,x);
+
 
 }
