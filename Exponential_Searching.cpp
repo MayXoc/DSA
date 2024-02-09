ff7226e48a4c74f7a969464797812da483dc4997
MayXoc <mayankbisht8126@gmail.com>
2024-02-09 22:29:03 +0530

    Exponential Searching Method

---
+++ Exponential_Searching.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int BinarySearch(vector <int> &arr,int l,int h,int x){
+    while(l<=h){
+        int mid = l + (h-l)/2;
+        if(arr[mid] == x || l==h){
+            return mid;
+        }
+        else if(arr[mid]>x){
+            h=mid-1;
+        }
+        else{
+            l = mid + 1;
+        }
+    }
+    return -1;
+} 
+
+void ExponentialSearch(vector <int> &arr,int x){
+    int n = arr.size() -1;
+    if(arr[0]==x){
+        cout<<"Element found at 0 index";
+    }
+    int i=1;
+    while(i<n && arr[i]<=x){
+        i=i*2;
+    }
+    int l=i/2;
+    int h=min(i,n);
+    cout<<"Element found at "<<BinarySearch(arr,l,h,x)<<" index";
+}
+
+int main(){
+    vector <int> arr{2,4,5,6,8,9,10,15,16,17,25,37,44,65,68,89};
+    int x = 89;
+    ExponentialSearch(arr,x);
+    
+}
