a35e96869c54a8c730790a599b62196dabbe4473
MayXoc <mayankbisht8126@gmail.com>
2024-02-13 00:05:51 +0530

    Code Corrected

--- EKO_SPOJ_Practice_Problem.cpp
+++ EKO_SPOJ_Practice_Problem.cpp
@@ -3,43 +3,41 @@
 #include <algorithm>
 using namespace std;
 
-bool ispossible(vector <long long int> &trees,long long int m,long long int mid){
+bool ispossible(vector<long long int> &trees, long long int m, long long int mid) {
     long long int woodCollected = 0;
-    for(int i= 0;i<trees.size();i++){
-        if(trees[i]>mid){
-            woodCollected = trees[i] - mid;
+    for (int i = 0; i < trees.size(); i++) {
+        if (trees[i] > mid) {
+            woodCollected += (trees[i] - mid); // Accumulate wood collected from all trees
         }
     }
-    return woodCollected >=m;
+    return woodCollected >= m;
 }
 
-long long int MaxSawBladeHeight(vector<long long int > &trees,long long int m){
-    long long int s=0 , e , ans = -1;
-    e = *max_element(trees.begin(),trees.end());
+long long int MaxSawBladeHeight(vector<long long int> &trees, long long int m) {
+    long long int s = 0, e, ans = -1;
+    e = *max_element(trees.begin(), trees.end());
 
-    while(s<=e){
-        long long int mid = s+(e-s)/2;
-        if(ispossible(trees,m,mid)){
-            ans = mid ; 
-            e = mid -1;
-        }
-        else{
+    while (s <= e) {
+        long long int mid = s + (e - s) / 2;
+        if (ispossible(trees, m, mid)) {
+            ans = mid;
             s = mid + 1;
+        } else {
+            e = mid - 1;
         }
     }
     return ans;
 }
 
-
-
-int main(){
-    long long int n,m;
-    cin>>n>>m;
-    vector <long long int> trees;
-    while(n--){
+int main() {
+    long long int n, m;
+    cin >> n >> m;
+    vector<long long int> trees;
+    while (n--) {
         long long int height;
-        cin>>height;
+        cin >> height;
         trees.push_back(height);
     }
-    cout<<MaxSawBladeHeight(trees,m);
+    cout << MaxSawBladeHeight(trees, m);
+    return 0;
 }
