23b418bda58ef044be6aba38ba2ccfa52939a344
MayXoc <mayankbisht8126@gmail.com>
2024-02-29 23:37:31 +0530

    LeetCode Problem Valid Plindrome

---
+++ Valid_Palindrome_LeetCode_problem.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool checkPalindrome(string s, int i, int j) {
+        while( i<= j) {
+            if(s[i] != s[j] ) {
+                return false;
+            }
+            else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while( i <= j) {
+            if(s[i] == s[j]) {
+                i++;
+                j--;
+            }
+            else{
+                //s[i]!=s[j]
+                //1 removal allowed
+                //check plaindrome for remaining string after removal
+
+                //ith character -> remove
+                bool ans1 = checkPalindrome(s, i+1, j);
+                //jth character -> remove
+                bool ans2 = checkPalindrome(s, i, j-1);
+
+                return ans1 || ans2;
+            }
+        }
+        //agar yha tk pohoche ho
+        //iska matlab valid palindrome hai
+        //iska matlab -> 0 removal
+        return true;
+    }
+};
